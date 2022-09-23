52acc5d0e0ae983ef7417b194332fae05dd7e02a
Samiur Prapon <samiur.prapon@gmail.com>
2022-09-23 16:14:09 +0600

    Reduce scripting

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Redis-Cluster
+
+Make executable
+```bash
+chmod +x redis.sh
+```
+Create a cluster
+```bash 
+./redis.sh
+```
--- redis.sh
+++ redis.sh
@@ -1,16 +1,9 @@
-cd ./9000 && redis-server redis.conf
+redis-server ./9000/redis.conf
+redis-server ./9001/redis.conf
+redis-server ./9002/redis.conf
+redis-server ./9003/redis.conf
+redis-server ./9004/redis.conf
+redis-server ./9005/redis.conf
+redis-server ./9006/redis.conf
 
-cd ../9001 && redis-server redis.conf
-
-cd ../9002 && redis-server redis.conf
-
-cd ../9003 && redis-server redis.conf
-
-cd ../9004 && redis-server redis.conf
-
-cd ../9005 && redis-server redis.conf
-
-cd ../9006 && redis-server redis.conf
-
-
-cd ../ && redis-cli --cluster create  127.0.0.1:9000 127.0.0.1:9001 127.0.0.1:9002 127.0.0.1:9003 127.0.0.1:9004 127.0.0.1:9005 127.0.0.1:9006 --cluster-replicas 1
+redis-cli --cluster create 127.0.0.1:9000 127.0.0.1:9001 127.0.0.1:9002 127.0.0.1:9003 127.0.0.1:9004 127.0.0.1:9005 127.0.0.1:9006 --cluster-replicas 1
